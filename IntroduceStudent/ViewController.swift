439de96b070271460a5c3ac5ed5c59646a9b654f
eshitashrawan <eshrawan@andrew.cmu.edu>
2023-01-24 11:36:53 -0500

    Initial Commit

---
+++ IntroduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by Eshita Shrawan on 1/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
